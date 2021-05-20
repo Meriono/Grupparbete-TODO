04822125945fcc1557f7a5ccd66532c8f2e253c5
Liliana Montini Pitra <72017001+Liliana-MP@users.noreply.github.com>
2021-05-20 10:55:17 +0200

    Added test to addTodo method (#10)
    * Develop (#8)
    
    * Try and get springboot running (#7)
    
    * Updated pom.xml with Flapdoodle and mongodb instead of jpa
    
    * added a localhost and port in application.properties
    
    * a small change
    
    * something went wrong
    
    Co-authored-by: Liliana Montini Pitra
    
    * added mockrepo in TodoServiceTest
    
    Co-authored-by: Hanna Edlund
    
    * changed the return value on TodoService and TodoController from String to Todo. Added a test to the addTodo Method
    
    Co-authored-by: Hanna Edlund

--- src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
+++ src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
@@ -1,11 +1,20 @@
 package com.example.grupparbetetodo.service;
 
+
+import com.example.grupparbetetodo.model.Todo;
+
 import com.example.grupparbetetodo.repository.TodoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 
 /**
  * created by Mimi Santana
@@ -41,5 +50,16 @@ public class TodoServiceTest {
 
     @Test
     void addTodo() {
+        String expectedTodo = "hejsan";
+        boolean expectedDone = false;
+
+        Todo saveTodo = new Todo();
+        saveTodo.setTodo(expectedTodo);
+        saveTodo.setDone(expectedDone);
+
+        when(mockRepo.save(any())).thenReturn(saveTodo);
+
+        Todo actualTodo = todoService.addTodo(saveTodo);
+        assertEquals(saveTodo.getTodo(), actualTodo.getTodo());
     }
 }
