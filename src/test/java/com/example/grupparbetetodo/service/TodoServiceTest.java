3ba192a9dc099bc16ad41e18048dd283141ddf43
Katri <suntribe2020@gmail.com>
2021-05-20 12:45:38 +0200

    Added a Unit test for getAll-method

--- src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
+++ src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
@@ -1,11 +1,19 @@
 package com.example.grupparbetetodo.service;
 
+import com.example.grupparbetetodo.model.Todo;
 import com.example.grupparbetetodo.repository.TodoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * created by Mimi Santana
@@ -29,6 +37,16 @@ public class TodoServiceTest {
 
     @Test
     void getAll() {
+        Todo dishes = new Todo(1L,"Do the dishes",true);
+        Todo garbage = new Todo(2L,"Take out the stinky garbage",false);
+        Todo laundry = new Todo(3L,"Do the laundry",true);
+        Todo cookDinner = new Todo(4L,"Cook delicious dinner",false);
+
+        when(mockRepo.findAll()).thenReturn(Arrays.asList(dishes, garbage, laundry, cookDinner));
+        List<Todo> actual = (List<Todo>) todoService.getAll();
+
+        assertEquals(4, actual.size());
+        assertNotEquals(5, actual.size());
     }
 
     @Test
