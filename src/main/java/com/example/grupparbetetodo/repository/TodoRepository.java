d969c432cf6ec64ff0f8347a5f60a5d2e4499085
Hanna Edlund <72083540+Meriono@users.noreply.github.com>
2021-05-20 13:47:59 +0200

    Test to view all todos thats done (#12)
    * added a method findAllByDone
    
    * added test methods for ''find all by done'' both true and false
    
    * wrote the test for findingByDone on both true and false
    
    Co-authored-by: suntribe2020 <72077137+suntribe2020@users.noreply.github.com>

--- src/main/java/com/example/grupparbetetodo/repository/TodoRepository.java
+++ src/main/java/com/example/grupparbetetodo/repository/TodoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+    Iterable<Todo> findAllByDone(boolean done);
 }
--- src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
+++ src/test/java/com/example/grupparbetetodo/service/TodoServiceTest.java
@@ -1,8 +1,6 @@
 package com.example.grupparbetetodo.service;
 
-
 import com.example.grupparbetetodo.model.Todo;
-
 import com.example.grupparbetetodo.repository.TodoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,19 +8,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
-
-/**
- * created by Mimi Santana
- * Date: 2021-05-19
- * Time: 16:00
- * Project: Grupparbete-TODO
- * Copyright: MIT
- */
 @ExtendWith(MockitoExtension.class)
 public class TodoServiceTest {
 
@@ -61,5 +55,29 @@ public class TodoServiceTest {
 
         Todo actualTodo = todoService.addTodo(saveTodo);
         assertEquals(saveTodo.getTodo(), actualTodo.getTodo());
+    }
+
+    // Get a list of all todos thats done = true
+    @Test
+    void findAllByDoneTrue() {
+        Todo expected = new Todo(1L, "feed the cat", true);
+
+        when(mockRepo.findAllByDone(true)).thenReturn(Arrays.asList(expected));
+        List<Todo> actual = (List<Todo>) todoService.findAllByDone(true);
+
+        verify(mockRepo).findAllByDone(true);
+        assertEquals(mockRepo.findAllByDone(true), actual);
+    }
+
+    // Get a list of all todos thats done = false aka not done
+    @Test
+    void findAllByDoneFalse() {
+        Todo expected = new Todo(1L, "clean up", false);
+
+        when(mockRepo.findAllByDone(false)).thenReturn(Arrays.asList(expected));
+        List<Todo> actual = (List<Todo>) todoService.findAllByDone(false);
+
+        verify(mockRepo).findAllByDone(false);
+        assertEquals(mockRepo.findAllByDone(false), actual);
     }
 }
